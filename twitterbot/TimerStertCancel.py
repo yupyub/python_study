df8bafc6426200a4599c5a95e7904505199cea81
yupyub <sungyup5003@gmail.com>
2019-08-27 14:05:38 +0000

    a

--- twitterbot/TimerStertCancel.py
+++ twitterbot/TimerStertCancel.py
@@ -1,1 +1,16 @@
-import threading
+import threading as th
+
+count = 0
+
+def on_timer():
+    global count
+    count+=1
+    print(count)
+    timer =  th.Timer(0.1,on_timer)
+    timer.start()
+    if count == 10:
+        print("Cancel Timer...")
+        timer.cancel()
+    
+print("Starting Timer...")
+on_timer();
