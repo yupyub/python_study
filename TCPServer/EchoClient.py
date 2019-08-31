30a47b3cc73557e4f00125466180ff8dbc375a5c
yupyub <sungyup5003@gmail.com>
2019-08-31 14:26:59 +0000

    a

---
+++ TCPServer/EchoClient.py
@@ -0,0 +1,30 @@
+import socket
+import sys
+
+if __name__ == "__main__":
+	if len(sys.argv) < 4:
+		print("{0} <Bind IP> <Server IP> <Message>".format(sys.args))
+		
+	else :
+		bindIP = sys.argv[1]
+		serverIP = sys.argv[2]
+		message = sys.argv[3]
+	
+	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # SOCK_STREAM은 TCP socket을 뜻함
+	sock.bind((bindIP,0))
+	
+	try:
+		sock.connect((serverIP,5425))	# 연결 요청
+		# echo 송신
+		sbuff = bytes(message,encoding = "utf-8")
+		sock.send(sbuff)
+		print("송신 : {0}".format(message))
+		# echo 수신
+		rbuff = sock.recv(1024)
+		received = str(rbuff,encoding = "utf-8")
+		print("수신 : {0}".format(received))
+	finally :
+		sock.close()
+
+		
+		
