89290e0745c6621912bfbfcc82410d749b22b523
yupyub <sungyup5003@gmail.com>
2019-10-07 12:42:29 +0000

    0

--- PyTorch/AutoGradient.py
+++ PyTorch/AutoGradient.py
@@ -1,14 +1,6 @@
 import torch
-"""
-x = torch.ones(2,2,requires_grad = True)
-print(x)
-y = x+2
-print(y)
-print(y.grad_fn)
-print(y*y)
-z = y*y*3
-out = z.mean()
-print(z,out)
+
+
 '''
 a = torch.randn(2,2)
 a = ((a*3)/(a-1))
@@ -19,16 +11,31 @@ print(a.requires_grad)
 b = (a*a).sum()
 print(b.grad_fn)
 '''
-out.backward() # same as out.backward(torch.tensor(1.))
+"""
+x = torch.ones(2,2,requires_grad = True)
+y = x+2
+
+print(y)
+print(x.grad_fn) # 사용자 정의 Tensor 이므로 .grad_fn 은 None 이다.
+print(y.grad_fn)
+print(y*y)
+
+z = y*y*3 # (1+2)*(1+2)*3=27
+out = z.mean()
+#print(z,out)
+
+out.backward() # same as out.backward(torch.tensor(1.)) (out 이 1개의 원소만을 갖기 때문)
 print(x.grad) # print d(out)/dx
 """
+
+# Gradients
 x = torch.randn(3,requires_grad = True)
 y=  x*2
 while y.data.norm() < 1000:
     y = y*2
 print(y)
-v = torch.tensor([0.1,1.0,0.0001],dtype = torch.float)
-y.backward(v)
+gradients = torch.tensor([0.1,1.0,0.0001],dtype = torch.float)
+y.backward(gradients)
 print(x.grad)
 print(x.requires_grad)
 print((x**2).requires_grad)
