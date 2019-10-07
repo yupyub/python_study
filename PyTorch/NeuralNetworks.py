c0c3f7f8e94faa4a6bcb0a76e270d3c5a029563c
yupyub <sungyup5003@gmail.com>
2019-10-07 06:38:20 +0000

    0

--- PyTorch/NeuralNetworks.py
+++ PyTorch/NeuralNetworks.py
@@ -1,4 +1,5 @@
 """
+http://swlock.blogspot.com/2018/07/pytorch-autograd-tutorial.html
 < NN 학습 과정 >
 1. 학습 가능한 매개변수(또는 가중치(weight))를 갖는 신경망을 정의
 2. 데이터셋(dataset) 입력을 반복
@@ -9,12 +10,16 @@
     <가중치(wiehgt) = 가중치(weight) - 학습율(learning rate) * 변화도(gradient)>
 """
 import torch
-import torch.nn as nn
+"""
+# torch.nn 은 미니-배치(mini-batch)만 지원함. torch.nn 패키지 전체는 하나의 샘플이 아닌, 샘플들의 미니-배치만을 입력으로 받는다.
+# 예를 들어, nnConv2D 는 nSamples x nChannels x Height x Width 의 4차원 Tensor를 입력으로 한다.
+# 만약 하나의 샘플만 있다면, input.unsqueeze(0) 을 사용해서 가짜 차원을 추가
+"""
+import torch.nn as nn 
 import torch.nn.functional as F
 
 
 class Net(nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
         # 예상되는 입력 크기는 32x32
@@ -22,7 +27,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(1, 6, 3) # 1-inChannel, 6-outChannels, 3-kernelSize
         self.conv2 = nn.Conv2d(6, 16, 3)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension * 16_channels
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -47,4 +52,23 @@ class Net(nn.Module):
 
 
 net = Net()
-print(net)
+# print(net)
+'''
+params = list(net.parameters())
+print(len(params))
+print(params[0].size()) # conv1's weight
+'''
+
+input = torch.randn(1,1,32,32)
+out = net(input)
+#net.zero_grad()
+#out.backward(torch.randn(1,10))
+output = net(input)
+target = torch.randn(10) # dummy target, for example
+target = target.view(1,-1) # make it the dame shape as output
+criterion = nn.MSELoss();
+loss = criterion(output,target)
+print(loss)
+print(loss.grad_fn)  # MSELoss
+print(loss.grad_fn.next_functions[0][0])  # Linear
+print(loss.grad_fn.next_functions[0][0].next_functions[0][0])  # ReLU
